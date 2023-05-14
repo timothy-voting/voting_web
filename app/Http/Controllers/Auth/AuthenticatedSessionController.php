e3209e02668c3101950ff1ff6a4c7866d22551c0
timothy kibalama <timkibalama@gmail.com>
2023-05-14 19:34:11 +0300

    added face comparison module

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -19,110 +19,111 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class AuthenticatedSessionController extends Controller
 {
-	/**
-	 * Display the login view.
-	 *
-	 * @return \Inertia\Response
-	 */
-	public function create(): \Inertia\Response
-	{
-		return Inertia::render('Auth/Login', [
-			'canResetPassword' => Route::has('password.request'),
-			'status' => session('status'),
-		]);
-	}
+    /**
+     * Display the login view.
+     *
+     * @return \Inertia\Response
+     */
+    public function create(): \Inertia\Response
+    {
+        return Inertia::render('Auth/Login', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
+    }
 
-	/**
-	 * Handle an incoming authentication request.
-	 *
-	 * @param LoginRequest $request
-	 * @return RedirectResponse
-	 * @throws ValidationException
-	 */
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param LoginRequest $request
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
 
-	public function store(LoginRequest $request): RedirectResponse
-	{
-		$request->authenticate();
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
 
-		$request->session()->regenerate();
+        $request->session()->regenerate();
 
-		return redirect()->intended(RouteServiceProvider::HOME);
-	}
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
 
-	/**
-	 * @throws ValidationException
-	 */
-	public function apiStore(LoginRequest $request): Response | StreamedResponse
-	{
-		$request->authenticate();
-		$user = $request->user();
-		if($request->has('finger_details') || $request->hasFile('face')) {
-			$bioAuth = false;
-			$storage_path = storage_path().'/app/';
-			if ($request->has('finger_details')) {
-				$bioAuth = false;
-			} elseif ($request->hasFile('face')) {
-				$path = $storage_path.$request->file('face')->store('temp_faces');
-				$face = Face::where('user_id', $user->id)->first();
-				$face_path = $storage_path.$face->path;
-				$socket = stream_socket_client('tcp://127.0.0.1:8001', $errno, $errstr, 30);
-				if ($socket) {
-					stream_socket_sendto($socket, $face_path.",".$path);
-					$bioAuth = (stream_get_contents($socket)=='true');
-				}
-			}
+    /**
+     * @throws ValidationException
+     */
+    public function apiStore(LoginRequest $request): Response | StreamedResponse
+    {
+        $request->authenticate();
+        $user = $request->user();
+        if($request->has('finger_details') || $request->hasFile('face')) {
+            $bioAuth = false;
+            $storage_path = storage_path().'/app/';
+            if ($request->has('finger_details')) {
+                $finger_details = explode(",", $request->finger_details);
+                $bioAuth = (intval($finger_details)>500);
+            } elseif ($request->hasFile('face')) {
+                $path = $storage_path.$request->file('face')->store('temp_faces');
+                $face = Face::where('user_id', $user->id)->first();
+                $face_path = $storage_path.$face->path;
+                $socket = stream_socket_client('tcp://127.0.0.1:8001', $errno, $errstr, 30);
+                if ($socket) {
+                    stream_socket_sendto($socket, $face_path.",".$path);
+                    $bioAuth = (stream_get_contents($socket)=='true');
+                }
+            }
 
-			if($bioAuth){
-        $token = $user->createToken('login');
-				return Response(['token' => $token->plainTextToken]);
-			}
-			$error = [
-				"message" => "Failed to authenticate biometricData.",
-				"errors" => [
-					"biometricData" => [
-						"Failed to authenticate biometricData."
-					]
-				]
-			];
-			return Response($error, 422);
-		}
+            if($bioAuth){
+                $token = $user->createToken('login');
+                return Response(['token' => $token->plainTextToken]);
+            }
+            $error = [
+                "message" => "Failed to authenticate biometricData.",
+                "errors" => [
+                    "biometricData" => [
+                        "Failed to authenticate biometricData."
+                    ]
+                ]
+            ];
+            return Response($error, 422);
+        }
 
-		$fingerprint = FingerPrint::where('user_id', $user->id)->first();
-		if($fingerprint == null){
-			$error = [
-				"message" => "You do not have biometric data.",
-				"errors" => [
-					"biometricData" => [
-						"You do not have biometric data."
-					]
-				]
-			];
-			return Response($error, 422);
-		}
+        $fingerprint = FingerPrint::where('user_id', $user->id)->first();
+        if($fingerprint == null){
+            $error = [
+                "message" => "You do not have biometric data.",
+                "errors" => [
+                    "biometricData" => [
+                        "You do not have biometric data."
+                    ]
+                ]
+            ];
+            return Response($error, 422);
+        }
 
-		return Storage::download($fingerprint->path, 'fingerprint.bin');
-	}
+        return Storage::download($fingerprint->path, 'fingerprint.bin');
+    }
 
-	/**
-	 * Destroy an authenticated session.
-	 *
-	 * @param Request $request
-	 * @return RedirectResponse
-	 */
-	public function destroy(Request $request): RedirectResponse
-	{
-		Auth::guard('web')->logout();
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
 
-		$request->session()->invalidate();
+        $request->session()->invalidate();
 
-		$request->session()->regenerateToken();
+        $request->session()->regenerateToken();
 
-		return redirect('/login');
-	}
+        return redirect('/login');
+    }
 
-	public function apiDestroy(Request $request): Response
-	{
-		$request->user()->currentAccessToken()->delete();
-		return Response(['token'=>null]);
-	}
+    public function apiDestroy(Request $request): Response
+    {
+        $request->user()->currentAccessToken()->delete();
+        return Response(['token'=>null]);
+    }
 }
