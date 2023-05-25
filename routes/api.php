d63e41cdb031fb013b188ce44c052aa1c5dd381d
timothy kibalama <timkibalama@gmail.com>
2023-05-26 02:00:10 +0300

    added refresh.sh

--- routes/api.php
+++ routes/api.php
@@ -23,24 +23,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [AuthenticatedSessionController::class, 'apiStore'])->name('api.login');
-Route::post('register', [RegisteredUserController::class, 'apiStore'])->name('api.register');
-Route::post('face', [FaceController::class, 'store'])->name('face');
-Route::post('fingerprint', [FingerPrintController::class, 'store'])->name('fingerprint');
-Route::get('is_student', function (Request $request){
-    $students = Student::where('student_no',$request->student_no)->get();
-    return Response(['isStudent'=>count($students) != 0]);
+Route::prefix('v1')->group(function () {
+    Route::post('login', [AuthenticatedSessionController::class, 'apiStore'])->name('api.login');
+    Route::post('register', [RegisteredUserController::class, 'apiStore'])->name('api.register');
+    Route::post('face', [FaceController::class, 'store'])->name('face');
+    Route::post('fingerprint', [FingerPrintController::class, 'store'])->name('fingerprint');
+    Route::get('is_student', function (Request $request) {
+        $students = Student::where('student_no', $request->student_no)->get();
+        return Response(['isStudent' => count($students) != 0]);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('logout', [AuthenticatedSessionController::class, 'apiDestroy'])->name('api.logout');
+        Route::get('positions', function () {
+            return Position::all();
+        });
+        Route::get('student', [StudentController::class, 'show'])->name('student');
+        Route::get('candidates', [CandidateController::class, 'show'])->name('candidates');
+        Route::post('votes', [VotingResultController::class, 'store'])->name('votes.store');
+    });
+
+
+    Route::get('votes', [VotingResultController::class, 'index'])->name('votes.index');
 });
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user', function (Request $request) { return $request->user();});
-    Route::post('logout', [AuthenticatedSessionController::class, 'apiDestroy'])->name('api.logout');
-    Route::get('positions', function (){return Position::all();});
-    Route::get('student', [StudentController::class, 'show'])->name('student');
-    Route::get('candidates', [CandidateController::class, 'show'])->name('candidates');
-    Route::post('votes',[VotingResultController::class, 'store'])->name('votes.store');
-});
-
-
-Route::get('votes',[VotingResultController::class, 'index'])->name('votes.index');
 
