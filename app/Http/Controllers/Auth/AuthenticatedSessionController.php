922013f0e97b72ac0c976ed2a22b634582bed18d
timothy kibalama <timkibalama@gmail.com>
2023-06-17 00:01:41 +0300

    fixed face_server restart

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -72,7 +72,7 @@ class AuthenticatedSessionController extends Controller
                     try {
                         $socket = stream_socket_client('tcp://127.0.0.1:8001', $errno, $errstr, 30);
                     } catch (\Exception $exception) {
-                        shell_exec('python '.base_path().'face_server.py &');
+                        shell_exec('python '.base_path().'/face_server.py &');
                         sleep(2);
                         continue;
                     }
